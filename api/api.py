709b79dda75e04431b5557ab9a8daff72e0f5c2e
jpark <jpark9013@gmail.com>
2020-07-26 15:41:18 -0400

    Imported all objects to main Client file

--- api/api.py
+++ api/api.py
@@ -1,6 +1,21 @@
 from api.apirequestmaker import CodeForcesRequestMaker
 
+from api.return_objects.blog_entry import BlogEntry
+from api.return_objects.comment import Comment
+from api.return_objects.contest import Contest
+from api.return_objects.hack import Hack
+from api.return_objects.member import Member
+from api.return_objects.party import Party
+from api.return_objects.problem import Problem
+from api.return_objects.problem_result import ProblemResult
+from api.return_objects.problem_statistics import ProblemStatistics
+from api.return_objects.ranklist_row import RanklistRow
+from api.return_objects.rating_change import RatingChange
+from api.return_objects.recent_action import RecentAction
+from api.return_objects.submission import Submission
+from api.return_objects.user import User
 
-class CodeForcesClient(CodeForcesRequestMaker):
-    pass
+
+class Client(CodeForcesRequestMaker):
+
 
