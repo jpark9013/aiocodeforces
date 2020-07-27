adfec2fec437f9a29c4e3de04685ae70822a086a
jpark <jpark9013@gmail.com>
2020-07-27 00:47:52 -0400

    Removed unnecessary imports

--- api/api.py
+++ api/api.py
@@ -4,11 +4,7 @@ from api.return_objects.blog_entry import BlogEntry
 from api.return_objects.comment import Comment
 from api.return_objects.contest import Contest
 from api.return_objects.hack import Hack
-from api.return_objects.hack import JudgeProtocol
-from api.return_objects.member import Member
-from api.return_objects.party import Party
 from api.return_objects.problem import Problem
-from api.return_objects.problem_result import ProblemResult
 from api.return_objects.problem_statistics import ProblemStatistics
 from api.return_objects.ranklist_row import RanklistRow
 from api.return_objects.rating_change import RatingChange
